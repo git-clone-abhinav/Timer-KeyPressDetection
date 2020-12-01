cad9d5eea8c6a792ea82a467ac0408d8dd77acb9
abhinavSingh <abhi.anu003@gmail.com>
2020-12-01 11:08:46 +0530

    loading + keylog

--- timer.py
+++ timer.py
@@ -1,7 +1,8 @@
 import time
 import keyboard
 import os
-seconds = 5
+seconds = 10
+id = 0
 
 def loading_screen():
     with open('ccnet.txt', 'r') as screen:
@@ -13,18 +14,22 @@ def loading_bar(seconds):
         percent = (loading * 100) // seconds
         print("\n")
         print("Press Enter to enter Setup")
-        print("<" + ("-" * loading) + ("" * (seconds + loading)) +">" + str(percent) + "%")
+        print("<" + ("✈ " * loading) + ("" * (seconds + loading)) +">")
         print("\n")
         time.sleep(1)
+        if keyboard.is_pressed("enter"):
+            id=1
+            os.system('cls' if os.name == "nt" else "clear")
+            break
         os.system('cls' if os.name == "nt" else "clear")
         loading_screen()
+        
 
 
 loading_screen()
 loading_bar(seconds)
 
-'''for i in range(timeout):
-    print("time remaining = ",timeout-i)
-    time.sleep(1)
-    if keyboard.is_pressed("enter"):
-        break'''
+if(id==1):
+    print("Entering CCThinClient Setup")
+elif(id==0):
+    print("Loading Client ..")
