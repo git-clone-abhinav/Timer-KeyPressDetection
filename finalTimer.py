bdab0d0b4c8f0459fe89f212f51480e55090beb4
abhinavSingh <abhi.anu003@gmail.com>
2020-12-01 19:38:55 +0530

    final changes done .

--- finalTimer.py
+++ finalTimer.py
@@ -1,30 +1,14 @@
-import sys, time, msvcrt
+import sys ,time ,keyboard
 
-def readInput( caption, default, timeout = 5):
+timeout = 5
+id = 0
+start_time = time.time()
 
-    start_time = time.time()
-    sys.stdout.write('%s(%s):'%(caption, default))
-    sys.stdout.flush()
-    input = ''
-    while True:
-        if msvcrt.kbhit():
-            byte_arr = msvcrt.getche()
-            if ord(byte_arr) == 13: # enter_key
-                break
-            elif ord(byte_arr) >= 32: #space_char
-                input += "".join(map(chr,byte_arr))
-        if len(input) == 0 and (time.time() - start_time) > timeout:
-            print("timing out, using default value.")
-            break
-
-    print('')  # needed to move to next line
-    if len(input) > 0:
-        return input
-    else:
-        return default
-
-# and some examples of usage
-ans = readInput('Please type a name', 'john') 
-print( 'The name is %s' % ans)
-ans = readInput('Please enter a number', 10 ) 
-print( 'The number is %s' % ans) 
+while True:
+    if keyboard.is_pressed("enter"):
+        id = 1
+        print("Break")
+        break
+    if id == 0 and (time.time() - start_time) > timeout:
+        print("timing out, using default value.")
+        break
