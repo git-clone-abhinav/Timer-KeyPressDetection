ba72708cd44d5c3c1f47b351330a1e5fe1f0c9b5
abhinavSingh <abhi.anu003@gmail.com>
2020-12-01 22:22:18 +0530

    printing menu

--- finalTimer.py
+++ finalTimer.py
@@ -1,35 +1,50 @@
-import sys ,time ,keyboard ,os
+import time, keyboard, os
 
 timeout = 5
 id = 0
 start_time = time.time()
 
+def printing_menu():
+    print("Press DEL to enter Setup..........")
+
 while True:
     
     if keyboard.is_pressed("del"):
-        #id = 1
         print("Break")
         break
-    if id == 0 and (time.time() - start_time) > timeout:
+    if id == 4 and (time.time() - start_time) > timeout:
         print("timing out, using default value.")
         break
     else:
+        
         if id == 0 and (time.time() - start_time) > 1:
-            print("Press DEL to enter Setup..........")
+            printing_menu()
             print("4")
             id = 1
+            if keyboard.is_pressed("del"):
+                print("Break")
+                break
         if id == 1 and (time.time() - start_time) > 2:
-            print("Press DEL to enter Setup..........")
+            printing_menu()
             print("3")
             id = 2
+            if keyboard.is_pressed("del"):
+                print("Break")
+                break
         if id == 2 and (time.time() - start_time) > 3:
-            print("Press DEL to enter Setup..........")
+            printing_menu()
             print("2")
             id = 3
+            if keyboard.is_pressed("del"):
+                print("Break")
+                break
         if id == 3 and (time.time() - start_time) > 4:
-            print("Press DEL to enter Setup..........")
+            printing_menu()
             print("1")
             id = 4
-    time.sleep(1)
-    os.system("cls")
+            if keyboard.is_pressed("del"):
+                print("Break")
+                break
+    #time.sleep(1)
+    #os.system("cls")
     
