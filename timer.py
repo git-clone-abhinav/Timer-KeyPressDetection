4e7769c921e00ee48d3d280fb510979d2f112d62
abhinavSingh <abhi.anu003@gmail.com>
2020-12-01 10:52:31 +0530

    LOADER

--- timer.py
+++ timer.py
@@ -1,12 +1,30 @@
 import time
 import keyboard
-timeout=10
+import os
+seconds = 5
 
-for i in range(timeout):
+def loading_screen():
+    with open('ccnet.txt', 'r') as screen:
+        for lines in screen:
+            print(lines, end='')
+
+def loading_bar(seconds):
+    for loading in range(0, seconds+1):
+        percent = (loading * 100) // seconds
+        print("\n")
+        print("Press Enter to enter Setup")
+        print("<" + ("-" * loading) + ("" * (seconds + loading)) +">" + str(percent) + "%")
+        print("\n")
+        time.sleep(1)
+        os.system('cls' if os.name == "nt" else "clear")
+        loading_screen()
+
+
+loading_screen()
+loading_bar(seconds)
+
+'''for i in range(timeout):
     print("time remaining = ",timeout-i)
     time.sleep(1)
     if keyboard.is_pressed("enter"):
-        break
-
-
-print("Time is UP")
+        break'''
