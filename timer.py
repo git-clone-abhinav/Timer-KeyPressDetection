ef3d31874e9ab874e16244273a3a605a8f4209af
abhinavSingh <abhi.anu003@gmail.com>
2020-12-01 09:32:56 +0530

    key log introduced

--- timer.py
+++ timer.py
@@ -1,10 +1,12 @@
 import time
-
+import keyboard
 timeout=10
 
 for i in range(timeout):
     print("time remaining = ",timeout-i)
     time.sleep(1)
-    
+    if keyboard.is_pressed("enter"):
+        break
+
 
 print("Time is UP")
